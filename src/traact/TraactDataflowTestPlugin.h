b87435e253eff4dc3301b07095afffb63d771304
Frieder Pankratz <frieder.pankratz@gmail.com>
2022-06-15 11:33:16 +0200

    new feature: port groups

--- src/traact/TraactDataflowTestPlugin.h
+++ src/traact/TraactDataflowTestPlugin.h
@@ -20,9 +20,9 @@ class DataflowTestComponent : public component::Component {
         pattern::Pattern::Ptr
             pattern = std::make_shared<pattern::Pattern>("DataflowTestComponent", traact::Concurrency::UNLIMITED,component::ComponentType::SYNC_FUNCTIONAL);
 
-        pattern->addConsumerPort("input0", Pose6DHeader::MetaType)
-            .addConsumerPort("input1", Pose6DHeader::MetaType)
-            .addProducerPort("output", Pose6DHeader::MetaType);
+        pattern->addConsumerPort("input0", Pose6DHeader::NativeTypeName)
+            .addConsumerPort("input1", Pose6DHeader::NativeTypeName)
+            .addProducerPort("output", Pose6DHeader::NativeTypeName);
 
         pattern->addCoordinateSystem("A", false)
             .addCoordinateSystem("B", false)
